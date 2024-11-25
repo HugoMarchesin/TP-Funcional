ad76fc50c25c5cf07bded6b3df58b8042b88ca62
HugoMarchesin <hugo.elio.marchesin@gmail.com>
2024-11-25 09:39:49 -0300

    Delete tp-funcional/src/test.hs

--- tp-funcional/src/test.hs
+++
@@ -1,9 +0,0 @@
-module Spec where
-import TP-funcional
-import Test.Hspec
-
-correrTests :: IO ()
-correrTests = hspec $ do
-  describe "Test para los hechizos" $ do
-    it "LagrimaFenix aumenta la salud del Mago" $ do
-      lagrimaFenix 40 mago1 `shouldBe` Mago "Gandulfo" 300 120 [lanzarLagrimaFenix 20, sectumSempra]
